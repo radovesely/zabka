02ca71b555516198b881e4af3372eeaa503f7798
Rado <rado.veselyy@gmail.com>
2018-06-02 12:50:28 +0200

    dropdown fixed, disaster avoid

--- js/galleryscript.js
+++ js/galleryscript.js
@@ -50,3 +50,24 @@ function bottomVisible() {
     const bottomOfPage = visible + scrollY >= pageHeight
     return bottomOfPage || pageHeight < visible
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
